850b45730bea229c7a414a634aa17a311a22d6ff
xtofl <kristoffel.pirard@midiagnostics.com>
2021-12-20 14:02:57 +0100

    Kiss: remove unneeded intermediate

--- src/fmappy/functor.py
+++ src/fmappy/functor.py
@@ -3,12 +3,9 @@ global fmap_vtable
 fmap_vtable = {}
 
 
-def fmap_for(type):
-    def register_fmap(func):
-        fmap_vtable[type] = func
-        return func
-
-    return register_fmap
+def register_functor(type, func):
+    fmap_vtable[type] = func
+    return func
 
 
 def fmap(fn, data):
@@ -23,11 +20,11 @@ apply = lambda fn, data: fn(data)
 construct_from_mapped = lambda fn, data: type(data)(map(fn, data))
 
 
-fmap_for(None)(lambda fn, data: None)
-fmap_for(int)(apply)
-fmap_for(str)(apply)
-fmap_for(float)(apply)
-fmap_for(bool)(apply)
-fmap_for(list)(construct_from_mapped)
-fmap_for(tuple)(construct_from_mapped)
-fmap_for(dict)(lambda fn, data: { key: fn(value) for key, value in data.items() })
+register_functor(None, lambda fn, data: None)
+register_functor(int, apply)
+register_functor(str, apply)
+register_functor(float, apply)
+register_functor(bool, apply)
+register_functor(list, construct_from_mapped)
+register_functor(tuple, construct_from_mapped)
+register_functor(dict, lambda fn, data: { key: fn(value) for key, value in data.items() })
