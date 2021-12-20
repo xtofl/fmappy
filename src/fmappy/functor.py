3c614c211897d4a2eb38bcc7fa3edbf6f1116c3c
xtofl <kristoffel.pirard@midiagnostics.com>
2021-12-20 13:25:00 +0100

    Take implementation from gist
    https://gist.github.com/3noch/eef18dba108be7db0441

--- src/fmappy/functor.py
+++ src/fmappy/functor.py
@@ -1,2 +1,28 @@
-def fmap(f, x):
-    return x
+
+global fmap_vtable
+fmap_vtable = {}
+
+
+def fmap_for(type):
+    def register_fmap(func):
+        fmap_vtable[type] = func
+        return func
+
+    return register_fmap
+
+
+def fmap(fn, data):
+    return fmap_vtable[type(data)](fn, data)
+
+
+apply = lambda fn, data: fn(data)
+
+
+fmap_for(None)(lambda fn, data: None)
+fmap_for(int)(apply)
+fmap_for(str)(apply)
+fmap_for(float)(apply)
+fmap_for(bool)(apply)
+fmap_for(list)(map)
+fmap_for(tuple)(map)
+fmap_for(dict)(lambda fn, data: { key: fn(value) for key, value in data.iteritems() })
