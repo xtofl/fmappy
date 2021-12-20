66e2412dced01e92ac30703b2903c9009e6ed6de
xtofl <kristoffel.pirard@midiagnostics.com>
2021-12-20 13:21:49 +0100

    Test: function is applied to elements of Functor

--- tests/test_functor.py
+++ tests/test_functor.py
@@ -22,3 +22,9 @@ SUPPORTED_TYPES = (
 def test_fmap_returns_same_type(type_, value):
     """When giving fmap an object, it returns an object of the same type."""
     assert isinstance(fmap(twice, value), type_)
+
+
+def test_fmap_applies_the_function():
+    assert fmap(twice, 1) == 2
+    assert fmap(twice, (1, 2, 3)) == (2, 4, 6)
+    assert fmap(twice, dict(a=1, b=2, c=3)) == dict(a=2, b=4, c=6)
